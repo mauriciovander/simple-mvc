17310023f62d0d0b05fea7dd6e4b40b37a027d07
Mauricio <mauriciovander@gmail.com>
2015-05-21 21:30:32 -0300

    Update bootloader.php

--- system/bootloader.php
+++ system/bootloader.php
@@ -2,11 +2,13 @@
 
 define('SYSTEM',__DIR__);
 define('CONFIG',realpath(SYSTEM.'/config'));
-define('INCLUDES',realpath(SYSTEM.'/includes'));
-define('MODELS',realpath(SYSTEM.'/models'));
-define('VIEWS',realpath(SYSTEM.'/views'));
 define('CLASSES',realpath(SYSTEM.'/classes'));
-define('CONTROLLERS',realpath(SYSTEM.'/controllers'));
+
+define('APPLICATION',realpath(SYSTEM.'../application'));
+define('MODELS',realpath(APPLICATION.'/models'));
+define('CONTROLLERS',realpath(APPLICATION.'/controllers'));
+define('VIEWS',realpath(APPLICATION.'/views'));
+define('INCLUDES',realpath(APPLICATION.'/includes'));
 
 require CONFIG.'/constants.php';
 

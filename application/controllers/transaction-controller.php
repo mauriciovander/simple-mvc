b453822260ac4cc437539d83afdece958ae0ca72
Mauricio <mauriciovander@gmail.com>
2015-05-25 18:42:38 -0300

    Create transaction-controller.php

---
+++ application/controllers/transaction-controller.php
@@ -0,0 +1,42 @@
+
+<?
+
+
+// https://blockchain.info/api/api_receive/
+
+
+class Transaction_Controller extends Base_Controller {
+	public function index(){
+    
+    // http://txuy.net/transaction?input_transaction_hash=82e4253a1995de994829e33f8c58ed9e81780f966078ef010b50fcb55ddca70a&shared=false&address=1DAKHEEzx2YGZF4tUdkJzt7TNHCteUpV9&destination_address=1DAKHEEzx2YGZF4tUdkJzt7TNHCteUpV9&input_address=1BBj9WEE7bYX8GWQRWwzRwFA7BM8MZhmpR&test=true&signature=qwertyui&anonymous=false&id=12345678&confirmations=0&value=4711363480&transaction_hash=82e4253a1995de994829e33f8c58ed9e81780f966078ef010b50fcb55ddca70a
+    
+    //input_transaction_hash=82e4253a1995de994829e33f8c58ed9e81780f966078ef010b50fcb55ddca70a
+    //shared=false
+    //address=1DAKHEEzx2YGZF4tUdkJzt7TNHCteUpV9
+    //destination_address=1DAKHEEzx2YGZF4tUdkJzt7TNHCteUpV9
+    //input_address=1BBj9WEE7bYX8GWQRWwzRwFA7BM8MZhmpR
+    //test=true
+    //signature=qwertyui
+    //anonymous=false
+    //id=12345678
+    //confirmations=0
+    //value=4711363480
+    //transaction_hash=82e4253a1995de994829e33f8c58ed9e81780f966078ef010b50fcb55ddca70a
+    
+    $transaction = new Transaction_Model();
+    $transaction->input_transaction_hash = $this->input_transaction_hash;
+    $transaction->shared = $this->shared;
+    $transaction->address = $this->address;
+    $transaction->destination_address = $this->destination_address;
+    $transaction->input_address = $this->input_address;
+    $transaction->test = $this->test;
+    $transaction->signature = $this->signature;
+    $transaction->anonymous = $this->anonymous;
+    $transaction->id = $this->id;
+    $transaction->confirmations = $this->confirmations;
+    $transaction->value = $this->value;
+    $transaction->transaction_hash = $this->transaction_hash;
+    $transaction->save();
+    
+	}
+}

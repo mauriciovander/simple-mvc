7cd3c8ef0b7fdfbe19477f68828d141f12adcd85
Mauricio <mauriciovander@gmail.com>
2015-05-25 19:09:15 -0300

    Update constants.php

--- system/config/constants.php
+++ system/config/constants.php
@@ -6,3 +6,10 @@ define('OS',php_uname('s'));
 
 define('EMAIL_USER','user@gmail.com');
 define('EMAIL_PASSWORD','**********');
+
+
+
+define('ADDRESS','1DAKHEEzx2YGZF4tUdkJzt7TNHCteUpV9');
+define('SECRET','41e8770714752e8dcdc8242085d65829f45eb96d');
+
+

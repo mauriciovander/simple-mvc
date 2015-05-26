a8e4557a5a7635c3ee25e567d70d5fb763c69f81
Mauricio <mauriciovander@gmail.com>
2015-05-26 15:06:33 -0300

    Update transaction-controller.php

--- application/controllers/transaction-controller.php
+++ application/controllers/transaction-controller.php
@@ -8,21 +8,6 @@
 class Transaction_Controller extends Base_Controller {
 	public function index(){
     
-	    // http://txuy.net/transaction?input_transaction_hash=82e4253a1995de994829e33f8c58ed9e81780f966078ef010b50fcb55ddca70a&shared=false&address=1DAKHEEzx2YGZF4tUdkJzt7TNHCteUpV9&destination_address=1DAKHEEzx2YGZF4tUdkJzt7TNHCteUpV9&input_address=1BBj9WEE7bYX8GWQRWwzRwFA7BM8MZhmpR&test=true&signature=qwertyui&anonymous=false&id=12345678&confirmations=0&value=4711363480&transaction_hash=82e4253a1995de994829e33f8c58ed9e81780f966078ef010b50fcb55ddca70a
-	    
-	    //input_transaction_hash=82e4253a1995de994829e33f8c58ed9e81780f966078ef010b50fcb55ddca70a
-	    //shared=false
-	    //address=1DAKHEEzx2YGZF4tUdkJzt7TNHCteUpV9
-	    //destination_address=1DAKHEEzx2YGZF4tUdkJzt7TNHCteUpV9
-	    //input_address=1BBj9WEE7bYX8GWQRWwzRwFA7BM8MZhmpR
-	    //test=true
-	    //signature=qwertyui
-	    //anonymous=false
-	    //id=12345678
-	    //confirmations=0
-	    //value=4711363480
-	    //transaction_hash=82e4253a1995de994829e33f8c58ed9e81780f966078ef010b50fcb55ddca70a
-	    
 	    $transaction = new Transaction_Model;
 	    $transaction->input_transaction_hash = $this->input_transaction_hash;
 	    $transaction->shared = $this->shared;
@@ -44,24 +29,12 @@ class Transaction_Controller extends Base_Controller {
 	
 	public function create(){
 		$transaction = new Transaction_Model;
-		$transaction->id = 'TEST_ID'; 
+		$transaction->id = sha1(uniqid()); 
 		$url = 'https://blockchain.info/api/receive';
 
-		// method=create
-		// cors=true
-		// format=plain
-		// address=1DAKHEEzx2YGZF4tUdkJzt7TNHCteUpV9
-		// shared=false
-		
 		$signature = sha1($transaction->id . SECRET);
 		
 		$callback_url = urlencode('http://'.$_SERVER['HTTP_HOST'].'/transaction?id='.$transaction->id.'&signature='.$signature);
-		
-		
-		$api_code = 'API_CODE';
-		
-		// $blockchain = new \Blockchain\Blockchain($api_code);
-		// $response = $blockchain->Receive->generate(ADDRESS, $callback_url);
 		
 		$root_url = 'https://blockchain.info/api/receive';
 		$parameters = 'method=create&address=' . ADDRESS .'&callback='. $callback_url;
@@ -76,21 +49,6 @@ class Transaction_Controller extends Base_Controller {
 		$transaction->save();
 		
 		echo 'Send coins to '.$object->input_address;
-		
-		// header('Content-Type: image/png');
-
-		// $qrCode = new Endroid\QrCode\QrCode;
-		// $qrCode
-		//     ->setText($object->input_address)
-		//     ->setSize(300)
-		//     ->setPadding(10)
-		//     ->setErrorCorrection('high')
-		//     ->setForegroundColor(array('r' => 0, 'g' => 0, 'b' => 0, 'a' => 0))
-		//     ->setBackgroundColor(array('r' => 255, 'g' => 255, 'b' => 255, 'a' => 0))
-		//     ->setLabel('Send coins to '.$object->input_address)
-		//     ->setLabelFontSize(16)
-		//     ->render()
-		// ;
 		
 		$this->log->addInfo($transaction);
 	}

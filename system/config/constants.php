98df69ad01a2a51255b8edc3fc850e0cf7071780
Mauricio <mauriciovander@gmail.com>
2015-05-26 16:24:16 -0300

    Update constants.php

--- system/config/constants.php
+++ system/config/constants.php
@@ -4,12 +4,9 @@ define('BACKEND',php_uname('n'));
 define('ARCHITECTURE',php_uname('m'));
 define('OS',php_uname('s'));
 
-define('EMAIL_USER','user@gmail.com');
-define('EMAIL_PASSWORD','**********');
-
-
-
-define('ADDRESS','1DAKHEEzx2YGZF4tUdkJzt7TNHCteUpV9');
-define('SECRET','41e8770714752e8dcdc8242085d65829f45eb96d');
-
+@define('EMAIL_USER','user@gmail.com');
+@define('EMAIL_PASSWORD','**********');
+@define('ADDRESS','BITCOIN-ADDRESS');
+@define('SECRET','super-secure-secret-key');
+@define('BLOCKCHAIN_API_KEY','BLOCKCHAIN-API-KEY');
 

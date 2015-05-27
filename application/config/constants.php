411159ee45a01d42befc6d13fa0b4aeb699def7a
Mauricio <mauriciovander@gmail.com>
2015-05-27 10:27:12 -0300

    Update constants.php

--- application/config/constants.php
+++ application/config/constants.php
@@ -1,12 +1,24 @@
 <?
 
+// general constants
 define('BACKEND',php_uname('n'));
 define('ARCHITECTURE',php_uname('m'));
 define('OS',php_uname('s'));
 
+// e-mail configuration
 @define('EMAIL_USER','user@gmail.com');
 @define('EMAIL_PASSWORD','**********');
-@define('ADDRESS','BITCOIN-ADDRESS');
+
+// Security 
 @define('SECRET','super-secure-secret-key');
+
+// Bitcoin configuration
+@define('ADDRESS','BITCOIN-ADDRESS');
 @define('BLOCKCHAIN_API_KEY','BLOCKCHAIN-API-KEY');
 
+// Mysql Configuration
+@define('MYSQL_HOST','MYSQL_HOST');
+@define('MYSQL_PORT','MYSQL_PORT');
+@define('MYSQL_USER','MYSQL_USER');
+@define('MYSQL_PASSWORD','MYSQL_PASSWORD');
+@define('MYSQL_DATABASE','MYSQL_DATABASE');

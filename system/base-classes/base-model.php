6155627d25159ae09eac44afa1d9dabe773b6642
Mauricio <mauriciovander@gmail.com>
2015-05-27 10:57:42 -0300

    Update base-model.php

--- system/base-classes/base-model.php
+++ system/base-classes/base-model.php
@@ -3,8 +3,9 @@
 interface Base_Model_Interface {}
 
 abstract class Base_Model implements Base_Model_Interface {
-  // error_log('Loading model '.get_called_class ());
-	public function __construct () {}
+	private $db;
+  	// error_log('Loading model '.get_called_class ());
+	public function __construct () { $this->db = DB::getInstance(); }
 	public function __destruct () {}
 	public function __toString(){ return json_encode($this); }
 	public function load() {}

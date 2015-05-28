b3f08afa069a2115389e56b96a874563e466781b
Mauricio <mauriciovander@gmail.com>
2015-05-27 22:36:55 -0300

    Update create.php

--- application/views/transaction/create.php
+++ application/views/transaction/create.php
@@ -1,5 +1,5 @@
 
-<img src="https://blockchain.info/qr?data=<?=$input_address?>&size=200" 
-      alt="Send coins to <?=$input_address?>" />
+<img src="https://blockchain.info/qr?data=<?php echo $input_address; ?>&size=200" 
+      alt="Send coins to <?php echo $input_address; ?>" />
 <br/>
-<p>Send coins to <?=$input_address?></p>
+<p>Send coins to <?php echo $input_address; ?></p>

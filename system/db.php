0d4c3fced58c6c3e5b33d60381e6d810e290cf51
Mauricio <mauriciovander@gmail.com>
2015-06-30 19:28:12 -0300

    Update db.php

--- system/db.php
+++ system/db.php
@@ -1,55 +1,82 @@
 <?php
 
 class DB_Exception extends Exception {
-	public function __construct($message, $error_code = 0, Exception $previous = null) {
-		parent::__construct ( $message, $error_code, $previous );
-		$this->log = new Monolog\Logger(get_called_class());
-		$this->log->pushHandler(new Monolog\Handler\StreamHandler(LOGS.'/app.log', Monolog\Logger::ERROR));
-		$this->log->addError($message);
-	}		
+
+    public function __construct($message, $error_code = 0, Exception $previous = null) {
+        parent::__construct($message, $error_code, $previous);
+        $this->log = new Monolog\Logger(get_called_class());
+        $this->log->pushHandler(new Monolog\Handler\StreamHandler(LOGS . '/app.log', Monolog\Logger::ERROR));
+        $this->log->addError($message);
+    }
+
 }
 
 // Singleton
 class DB {
-  private static $instance = null;
-  protected $dbh;
-  
-  private function __construct() { 
-    try{
-      $connection_string = 'mysql:host=' . MYSQL_HOST . ';port=' . MYSQL_PORT . ';dbname=' . MYSQL_DATABASE;
-      $this->dbh = new PDO($connection_string, MYSQL_USER, MYSQL_PASSWORD, array( PDO::ATTR_PERSISTENT => false));  
+
+    private static $instance = null;
+    private $dbh;
+    private $lastId;
+
+    private function __construct($db_config){
+        try {
+            $connection_string = 'mysql:host=' . $db_config::HOST . ';port=' . $db_config::PORT. ';dbname=' . $db_config::NAME;
+            $this->dbh = new PDO($connection_string, $db_config::USER, $db_config::PASSWORD);
+        } catch (PDOException $e) {
+            throw new Controller_Exception('Connection Error', 1, $e);
+        }
     }
-    catch (PDOException $e) {
-      throw new Controller_Exception('Connection Error', 1, $e);
+
+    static function getInstance($db_config) {
+        if (is_null(self::$instance)) {
+            self::$instance = new DB($db_config);
+        }
+        return self::$instance;
     }
-  }
-  
-  static function getInstance() {
-    if(is_null(self::$instance)) self::$instance = new DB;
-    return self::$instance;
-  }  
-  
-  public function __destruct(){
-     $this->dbh = null;
-  }
-  
-  public function execute($sql,Array $params = array()) {
-    try{
-      $stmt = $this->dbh->prepare($sql);
-      foreach($params as $n=>$v) {
-        $stmt->bindValue($n+1, $v);
-      }
-      if(!$stmt->execute()){
-        unset($stmt);
-        throw new Controller_Exception($stmt->errorInfo(), 1, $e);
-      }
-      $rows = $stmt->fetchAll();
-      unset($stmt);
-      return $rows;
+
+    public function __destruct() {
+        $this->dbh = null;
     }
-    catch (PDOException $e) {
-      throw new Controller_Exception($e->getMessage(), 1, $e);
+
+    public function lastId() {        
+        return $this->lastId;
     }
-  }
-  
+
+    public function begin() {
+        $this->dbh->beginTransaction();
+    }
+
+    public function commit() {
+        $this->dbh->commit();
+    }
+
+    public function rollBack() {
+        $this->dbh->rollBack();
+    }
+
+    public function execute($sql, Array $params = array()) {
+//        die( $this->debug($sql, $params));
+        try {
+            $sth = $this->dbh->prepare($sql);
+            $sth->execute($params);
+            $this->lastId = $this->dbh->lastInsertId();            
+            $result = $sth->fetchAll(PDO::FETCH_OBJ);
+            return $result;
+        } catch (PDOException $e) {
+            throw new DB_Exception('SQL ERROR: ' . $this->debug($sql, $params), 1, $e);
+        }
+        return false;
+    }
+
+    private function debug($statement, array $params = []) {
+        $statement = preg_replace_callback(
+                '/[?]/', function ($k) use ($params) {
+            static $i = 0;
+            return sprintf("'%s'", $params[$i++]);
+        }, $statement
+        );
+
+        echo '<pre>Query Debug:<br>', $statement, '</pre>';
+    }
+
 }

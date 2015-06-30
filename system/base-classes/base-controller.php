f4de131b8dcd57cd4d1e53e9e25ab58927780bb6
Mauricio <mauriciovander@gmail.com>
2015-06-30 19:30:06 -0300

    Update base-controller.php

--- system/base-classes/base-controller.php
+++ system/base-classes/base-controller.php
@@ -1,64 +1,85 @@
 <?php
 
-interface Base_Controller_Interface { public function index(); }
+interface Base_Controller_Interface {
+
+}
 
 class Controller_Exception extends Exception {
-	public function __construct($message, $error_code = 0, Exception $previous = null) {
-		parent::__construct ( $message, $error_code, $previous );
-		$this->log = new Monolog\Logger(get_called_class());
-		$this->log->pushHandler(new Monolog\Handler\StreamHandler(LOGS.'/app.log', Monolog\Logger::ERROR));
-		$this->log->addError($message);
-	}		
+
+    public function __construct($message, $error_code = 0, Exception $previous = null) {
+        parent::__construct($message, $error_code, $previous);
+        $this->log = new Monolog\Logger(get_called_class());
+        $this->log->pushHandler(new Monolog\Handler\StreamHandler(LOGS . '/app.log', Monolog\Logger::ERROR));
+        $this->log->addError($message);
+    }
+
 }
 
-class Controller_Success extends Controller_Success { }
+class Controller_Success extends Controller_Exception { }
 
-abstract class Data_Object implements JsonSerializable{
-	public $value;
-	public $method;
-	public $type;
-	public function __construct($value) { $this->value = $value; }
-	public function jsonSerialize() { return $this->value; }
-	public function __sleep() { return array('value','method','type'); }
-	public function __toString() { return $this->value; }
+class Input_Data_Object extends Data_Object {
+
+    public $type = 'input';
+
 }
 
-class Input_Data_Object extends Data_Object { public $type = 'input'; }
-class Output_Data_Object extends Data_Object { public $type = 'output'; }
+class Output_Data_Object extends Data_Object {
+
+    public $type = 'output';
+
+}
 
 abstract class Base_Controller implements Base_Controller_Interface {
 
-	private $input;
-	private $output;
-	private $router;
-	protected $log;
+    protected $input;
+    protected $output;
+    protected $router;
+    protected $log;
 
-	public function __toString() { return json_encode($this->output); }
-	public function __set($k,$v) { $this->input->{$k} = new Input_Data_Object($v); }
-	public function __get($k) {
-		if(isset($this->input->{$k})) return $this->input->{$k};
-		else throw new Controller_Exception(get_called_class ()."->$k is not defined", 1);
-	}
-
-	public function __destruct () {}
-	public function __construct (Router $router) {
-	  $this->router = $router;
-		$this->input = new StdClass();
-		$this->output = new StdClass();
-		$this->log = new Monolog\Logger(get_called_class());
-		$this->log->pushHandler(new Monolog\Handler\StreamHandler(LOGS.'/app.log', Monolog\Logger::INFO));
-	}
-
-  public function render(Array $data = array(), $template_path = null) {
-    foreach($data as $k => $v) $$k = $v;
-    if(is_null($template_path)) $template_path = $this->router->controller.'/'.$this->router->action.'.php';
-    try {
-      include(VIEWS.'/'.$template_path);
+    public function __toString() {
+        return json_encode($this->output);
     }
-    catch(Exception $e) {
-      throw new Controller_Exception('Missing template at '.$template_path, 0, $e);
+
+    public function __set($k, $v) {
+        $this->input->{$k} = new Input_Data_Object($v);
     }
-  }
-  
-	public function bypass() { $this->output = clone $this->input; }
+
+    public function __get($k) {
+        if (isset($this->input->{$k})) {
+            return $this->input->{$k};
+        } else {
+            throw new Controller_Exception(get_called_class() . "->$k is not defined", 1);
+        }
+    }
+
+    public function __destruct() {
+        
+    }
+    
+    public function __construct(Router $router) {
+        $this->router = $router;
+        $this->input = new StdClass();
+        $this->output = new StdClass();
+        $this->log = new Monolog\Logger(get_called_class());
+        $this->log->pushHandler(new Monolog\Handler\StreamHandler(LOGS . '/app.log', Monolog\Logger::INFO));
+    }
+
+    public function render(Array $data = array(), $template_path = null) {
+        foreach ($data as $k => $v) {
+            $$k = $v;
+        }
+        if (is_null($template_path)) {
+            $template_path = $this->router->controller . '/' . $this->router->action . '.php';
+        }
+        try {
+            include(VIEWS . '/' . $template_path);
+        } catch (Exception $e) {
+            throw new Controller_Exception('Missing template at ' . $template_path, 0, $e);
+        }
+    }
+
+    public function bypass() {
+        $this->output = clone $this->input;
+    }
+
 }
